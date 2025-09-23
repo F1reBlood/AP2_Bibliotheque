c83f4f59cb0c65666e8c97cb373f84b247f9c250
FireBlood <tristan.turki4@gmail.com>
2025-09-23 11:44:36 +0200

    Création des 3 classes principales

--- src/Model/Auteur.java
+++ src/Model/Auteur.java
@@ -1,4 +1,20 @@
 package Model;
 
+import java.sql.Date;
+
 public class Auteur {
+    private String id;
+    private String nom;
+    private String prenom;
+    private Date date_naissance;
+    private String description;
+
+    public Auteur(String id, String nom, String prenom, Date date_naissance, String description) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.date_naissance = date_naissance;
+        this.description = description;
+
+    }
 }
--- src/Model/Livre.java
+++ src/Model/Livre.java
@@ -1,4 +1,18 @@
 package Model;
 
 public class Livre {
+    private String ISBN;
+    private String titre;
+    private float prix;
+    private String adherantId;
+    private Auteur auteur;
+
+    public Livre(String ISBN, String titre, float prix, String adherantId, Auteur auteur) {
+        this.ISBN = ISBN;
+        this.titre = titre;
+        this.prix = prix;
+        this.adherantId = adherantId;
+        this.auteur = auteur;
+
+    }
 }
