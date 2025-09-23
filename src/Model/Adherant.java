7dfa75f22c7ab223b0917ae7cbe22814eebbfbc1
FireBlood <tristan.turki4@gmail.com>
2025-09-23 12:01:51 +0200

    ajout de la variable listLivres dans Adherant

--- src/Model/Adherant.java
+++ src/Model/Adherant.java
@@ -1,15 +1,19 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Adherant {
     private String identifiant;
     private String nom;
     private String prenom;
     private String email;
+    private ArrayList<Livre> listLivres;
 
-    public Adherant(String identifiant, String nom, String prenom, String email) {
+    public Adherant(String identifiant, String nom, String prenom, String email, ArrayList<Livre> listLivres) {
         this.identifiant = identifiant;
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
+        this.listLivres = listLivres;
     }
 }
