0f394e5cc19beb7e0ad1bffecb637630a57a11b8
FireBlood <tristan.turki4@gmail.com>
2025-09-23 11:38:40 +0200

    test

--- src/Model/Adherant.java
+++ src/Model/Adherant.java
@@ -1,4 +1,15 @@
 package Model;
 
 public class Adherant {
+    private String identifiant;
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Adherant(String identifiant, String nom, String prenom, String email) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
 }
